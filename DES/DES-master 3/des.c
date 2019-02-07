1d7a2126fae1fa19deb4341eab1d6480a10ae894
jeb-ubuntu <www.wangqiuyue2016@gmail.com>
2019-02-07 23:34:54 +0800

    add

--- DES/DES-master 3/des.c
+++ DES/DES-master 3/des.c
@@ -303,53 +303,45 @@ uint64_t des(uint64_t input, uint64_t key, char mode) {
     
 }
 
+void str2hex(char *source,char *dest,int keyLen){
+    uint8_t i;  
+    uint8_t highByte, lowByte;  
+  
+    for (i = 0; i < keyLen; i++)  
+    {  
+        highByte = source[i] >> 4;  
+        lowByte = source[i] & 0x0f ;  
+  
+        highByte += 0x30;  
+  
+        if (highByte > 0x39)  
+                dest[i * 2] = highByte + 0x07;  
+        else  
+                dest[i * 2] = highByte;  
+  
+        lowByte += 0x30;  
+        if (lowByte > 0x39)  
+            dest[i * 2 + 1] = lowByte + 0x07;  
+        else  
+            dest[i * 2 + 1] = lowByte;  
+    }  
+    return ;  
+}
+
+
 int main(int argc, const char * argv[]) {
 
     int i;
     
     uint64_t input = 0x9474B8E8C73BCA7D;
     uint64_t key = 0x0000000000000000;
-    uint64_t result = input;
     
-    /*
-     * TESTING IMPLEMENTATION OF DES
-     * Ronald L. Rivest 
-     * X0:  9474B8E8C73BCA7D
-     * X16: 1B1A2DDB4C642438
-     *
-     * OUTPUT:
-     * E: 8da744e0c94e5e17
-     * D: 0cdb25e3ba3c6d79
-     * E: 4784c4ba5006081f
-     * D: 1cf1fc126f2ef842
-     * E: e4be250042098d13
-     * D: 7bfc5dc6adb5797c
-     * E: 1ab3b4d82082fb28
-     * D: c1576a14de707097
-     * E: 739b68cd2e26782a
-     * D: 2a59f0c464506edb
-     * E: a5c39d4251f0a81e
-     * D: 7239ac9a6107ddb1
-     * E: 070cac8590241233
-     * D: 78f87b6e3dfecf61
-     * E: 95ec2578c2c433f0
-     * D: 1b1a2ddb4c642438  <-- X16
-     */
-    for (i = 0; i < 16; i++) {
-        
-        if (i%2 == 0) {
-            
-            result = des(result, result, 'e');
-            printf ("E: %016llx\n", result);
-            
-        } else {
-            
-            result = des(result, result, 'd');
-            printf ("D: %016llx\n", result);
-            
-        }
-    }
-    
+    char * in = "qwertyui";
+    char * in_hex[17];
+    in_hex[16]=0;
+    str2hex(in,in_hex,8);
+
+    printf("%x",in_hex);
     //result = des(input, key, 'e');
     //printf ("E: %016llx\n", result);
     
